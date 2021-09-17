89259ccbe89c69dce8f9934ba06ca25a546c03d3
mattjmason <masonmj10@gmail.com>
2021-09-17 00:19:14 -0400

    commit tree b1f1979fbd88f2b15aa8b730a432b407d32f22d5

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(timeString) {
+  const hour = ParseInt(timeString, 10);
+  if (hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
